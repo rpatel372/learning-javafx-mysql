5d245ddf1844dc5a575ede4d44fff181870f4043
Reema Patel <reemapatel97@gmail.com>
2019-01-15 21:38:27 -0500

    Added basic GUI and simple SQL connection

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,14 +1,26 @@
 package sample;
 
+import connectivity.ConnectionClass;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Controller {
     public TextField textfield;
     public Label textLabel;
 
-    public void button(ActionEvent actionEvent) {
-        textLabel.setText(textfield.getText());
+    public void button(ActionEvent actionEvent) throws SQLException {
+        ConnectionClass connectionClass = new ConnectionClass();
+        Connection connection = connectionClass.getConnection();
+
+        String sql= "INSERT INTO USER VALUES('" + textfield.getText() + "')";
+        Statement statement = connection.createStatement();
+        statement.executeUpdate(sql);
+
+        //textLabel.setText(textfield.getText());
     }
 }
